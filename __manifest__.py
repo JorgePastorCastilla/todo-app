6f73a913aee50f54ea8c97984e815b19c772927b
JorgePastorCastilla <jordi131313@gmail.com>
2020-02-14 19:30:02 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Jorge Pastor',
+    'depends': ['mail'],
+    'data': ['views/todo_view.xml'],
+    'application': True,
+}
